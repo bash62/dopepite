b7a2f1aa8ebfdbfd6b941d4ff9dd3897fce419e7
Hokanosekai <69720899+Hokanosekai@users.noreply.github.com>
2022-02-08 02:20:10 +0100

    Rework on design 🎨

--- assets/js/Components/RessourceSearch.vue
+++ assets/js/Components/RessourceSearch.vue
@@ -1,76 +1,53 @@
-
-
-
 <template>
-
     <div>
-
-      <div class=" border ">
-        <h1 class="flex  items-center justify-center text-2xl block mt-2">Cherche une ressource </h1>
+      <div class="">
         <div class="flex items-center justify-center">
           <input
               autofocus
               :placeholder="searchPlaceholder"
-              v-model="query" type="text" class="  my-4 w-2/6" id="search-bar">
+              v-model="query" type="text" class="py-3 px-6 text-xl text-zinc-900 text-center rounded-xl my-4 w-1/3" id="search-bar">
         </div>
-
       </div>
 
-      <div class=" flex justify-center text-xl font-bold py-3">
+      <div class=" flex justify-center text-xl font-bold py-4">
         <div v-if=" getFilteredPosts.length > 0">
           {{ getFilteredPosts.length }} ressources jamais renseigné trouvés.
-
         </div>
 
         <div v-else >
-          {{ getFilteredPosts.length }} ressources trouvé .
-          <a v-bind:href="newPost" class="text-blue-600"> La renseigner ? </a>
-
+          {{ getFilteredPosts.length }} ressources trouvé.
+          <a v-bind:href="newPost" class="text-blue-600">La renseigner ?</a>
         </div>
       </div>
 
       <div class="flex justify-center">
-        <div class="w-1/3">
-        <div v-if="this.query.length < 2"> </div>
+        <div class="md:w-2/5 w-4/5">
+          <div v-if="this.query.length < 0"></div>
 
-          <div v-else class=" justify-center items-center bg-gray-800 bg-opacity-75 p-4 m-1.5 rounded-sm cursor-pointer hover:scale-125 hover:my-2"  v-on:click="onClickRedirect" v-for="ressource in getFilteredPosts" v-bind:key="ressource.name" v-bind:href="ressource.id" >
-            <div class="relative flex text-align">
-              <a v-bind:href="url + ressource.id" class=" w-full h-full absolute "></a>
-              <div class=" left-5 " >
-                <div class=" bg-white rounded-full w-12 h-12 flex justify-center items-center mr-5">
-                  <div class=" text-3xl text-purple-800 font-black ">{{getFirstLetter(ressource.name)}}</div>
-                </div>
-
+          <div v-else class="justify-center items-center bg-zinc-700 bg-opacity-75 m-3 rounded-lg cursor-pointer hover:bg-zinc-800 h-16" v-on:click="onClickRedirect" v-for="ressource in getFilteredPosts" v-bind:key="ressource.name" v-bind:href="ressource.id" >
+            <div class="relative flex text-align h-full">
+              <a v-bind:href="url + ressource.id" class="w-full h-full absolute"></a>
+              <div class="bg-white rounded-lg w-16 h-full flex justify-center items-center" >
+                <div class=" text-3xl text-purple-800 font-black">{{getFirstLetter(ressource.name)}}</div>
               </div>
-              <div class="text-4xl font-medium text-white ">{{ ressource.name }}</div>
-        </div>
 
-
-
-
+              <p class="text-3xl font-medium text-white align-right h-full flex justify-center truncate items-center w-full px-4">{{ ressource.name.length > 40? ressource.name.substring(0, 40)+ "..." : ressource.name }}</p>
+            </div>
           </div>
-
         </div>
       </div>
-
-
-
     </div>
-
 </template>
 
 
 <script>
-
-
-
-export default {
+  export default {
     name: "ressourcesearch",
     data() {
       return {
         ressources: JSON.parse(this.res),
         query: '',
-        searchPlaceholder : "Nom d'une ressource",
+        searchPlaceholder : "Rechercher une ressource",
         url:'/ressource/add/',
         newPost:'/ressource/new'
       }
@@ -84,7 +61,6 @@ export default {
         return(name[0])
 
       }
-
     },
     mounted() {
 
@@ -95,12 +71,10 @@ export default {
           if(this.query == 'oe' ){
             this.query = "Œ";
           }
-
           return post.name.toLowerCase().startsWith(this.query.toLowerCase());
         })
       },
 
     },
   };
-
 </script>
